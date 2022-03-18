e11e3f8a7b232cb4f22c2ce91a15a73d911381f1
liviajp020 <liviajp.talenta@gmail.com>
2022-03-18 17:40:20 +0700

    add KabelPutus animation, add rintangan3(yuyul finish), add bocil animation, lose animation

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public float speed=1.0f;
+    public Animator animator;
    
     public void Update()
     {
@@ -18,5 +20,21 @@ public class Player : MonoBehaviour
     {
         GetComponent<Animator>().SetBool("Dead", true);
         GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
+       
+    }
+
+    public void WLose()
+    {
+        if (GetComponent<Animator>().GetBool("Dead"))
+            animator.SetBool("Lose", true);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            animator.SetBool("Win", true);
+        }
+       
     }
 }
